c77f03ce971647a4ac911d46c05d2f410963ec64
Kevin Bedi <kevin.m.bedi@gmail.com>
2013-05-14 18:33:20 -0400

    Begin tests for main omni.js file

--- test/omni.js
+++ test/omni.js
@@ -14,10 +14,35 @@ describe("Omni", function() {
 
     describe("Listen Method", function() {
         var server;
-        var expressSpy;
+
+        var expressStub;
+        var expressStaticStub;
+        var expressUseStub;
+        var expressGetStub;
+
+        var httpStub;
+        var httpListenSpy;
 
         before(function(done) {
-            // expressSpy = sinon.spy();
+            var express = require("express");
+
+            httpListenSpy = sinon.spy();
+
+            httpStub = sinon.stub(require("http"), "createServer").returns({
+                listen: httpListenSpy
+            });
+
+            expressStaticStub = sinon.stub();
+            expressUseStub = sinon.stub();
+            expressGetStub = sinon.stub();
+            expressStub = sinon.stub().returns({
+                use: expressUseStub,
+                get: expressGetStub
+            });
+
+            expressStub.static = expressStaticStub;
+
+            require.cache[require.resolve("express")].exports = expressStub;
 
             var testCollection = new Omni.Collection([{
                 label: "a"
@@ -34,6 +59,7 @@ describe("Omni", function() {
         });
 
         it("should return an express server listening on the specified port", function() {
+            console.log(expressStub);
             // expressSpy.calledOnce.should.be.true;
         });
     });
